28e621e8e6435a8f3f3f6f2ac53e01191062c8e8
Rafael Akiyama <rafael.akiyama@tqi.com.br>
2023-11-14 21:16:48 -0300

    Atualização do Projeto

--- cypress/e2e/TestesPesquisa.cy.js
+++ cypress/e2e/TestesPesquisa.cy.js
@@ -3,10 +3,18 @@
 
 import Pages from '../support/Pages/metodos';
 
+describe('CT 1 - Fazer Uma Pesquisa Com Sucesso', () => {
+   it('Fazer uma pesquisa com sucesso', () => {
+      Pages.acessarPagBlogDoAgi();
+      Pages.fazerUmaPesquisaComSucesso();
+   })
+
 describe('CT 2 - Fazer Uma Pesquisa Sem Sucesso', () => {
    it('Fazer uma pesquisa sem sucesso', () => {
       Pages.acessarPagBlogDoAgi();
       Pages.fazerUmaPesquisaSemSucesso();
    })
+   
+})   
 
 })
